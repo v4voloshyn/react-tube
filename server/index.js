9ce9555c394287e3067e8759f8d1b9baa0368ba3
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-08-23 12:22:13 +0300

    Basic setup config of server side

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
-
 const PORT = process.env.PORT || 5432;
 
-app.listen(PORT, () => console.log(`Server is running on ${PORT} PORT`));
+const connectDB = async () => {
+	try {
+		const connection = await mongoose.connect(process.env.MONGO_URI);
+		console.log(`MongoDB connected: ${connection.connection.host}`);
+	} catch (error) {
+		console.log(`Error : ${error.message}`);
+		process.exit(1);
+	}
+};
+
+app.listen(PORT, connectDB(), () => console.log(`Server is running on ${PORT} PORT`));
