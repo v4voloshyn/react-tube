8d18b1f9a939924375df0ef191ebeba849f561de
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-09-09 13:14:05 +0300

    Feat: add userRouter and userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,1 +1,84 @@
-export const testFN = (req, res) => res.json('Success!');
+import { createError } from '../helpers/error.js';
+import UserModel from '../models/User.js';
+
+export const updateUser = async (req, res, next) => {
+	if (req.params.id === req.user.id) {
+		try {
+			const updatedUser = await UserModel.findByIdAndUpdate(
+				req.user.id,
+				{
+					$set: req.body,
+				},
+				{
+					new: true,
+				}
+			);
+
+			res.status(200).json(updatedUser);
+		} catch (error) {}
+	} else {
+		return next(createError(403, 'You can not update this account'));
+	}
+};
+
+export const deleteUser = async (req, res, next) => {
+	if (req.params.id === req.user.id) {
+		try {
+			const deletedUser = await UserModel.findByIdAndDelete(req.user.id);
+			if (!deletedUser) {
+				return next(createError(404, 'User not found'));
+			}
+			res.status(200).json(`User ${deletedUser.name} has been deleted`);
+		} catch (error) {}
+	} else {
+		return next(createError(403, 'You can not delete on this account'));
+	}
+};
+
+export const getUser = async (req, res, next) => {
+	try {
+		const user = await UserModel.findById(req.params.id).select('-password');
+		if (!user) {
+			return next(createError(404, 'User not found'));
+		}
+		return res.status(200).json(user);
+	} catch (error) {
+		return next(createError(500, 'Something went wrong'));
+	}
+};
+
+export const subscribeUser = async (req, res, next) => {
+	try {
+		await UserModel.findByIdAndUpdate(req.user.id, {
+			$push: { subscribedUsers: req.params.id },
+		});
+
+		await UserModel.findByIdAndUpdate(req.params.id, {
+			$inc: { subscribers: 1 },
+		});
+
+		res.status(200).json('Successfull subscribe');
+	} catch (error) {
+		return next(createError(500, 'Something went wrong'));
+	}
+};
+
+export const unsubscribeUser = async (req, res, next) => {
+	try {
+		await UserModel.findByIdAndUpdate(req.user.id, {
+			$pull: { subscribedUsers: req.params.id },
+		});
+
+		await UserModel.findByIdAndUpdate(req.params.id, {
+			$inc: { subscribers: -1 },
+		});
+
+		res.status(200).json('Successfull UNsubscribe');
+	} catch (error) {
+		return next(createError(500, 'Something went wrong'));
+	}
+};
+
+export const like = async (req, res, next) => {};
+
+export const dislike = async (req, res, next) => {};
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,6 +1,28 @@
 import express from 'express';
-import { testFN } from '../controllers/userController.js';
+import {
+	deleteUser,
+	dislike,
+	getUser,
+	like,
+	subscribeUser,
+	unsubscribeUser,
+	updateUser,
+} from '../controllers/userController.js';
+import { verifyUserToken } from '../middlewares/authMiddleware.js';
 
 export const userRouter = express.Router();
 
-userRouter.get('/test', testFN);
+// GET USER
+userRouter.get('/find/:id', getUser);
+// UPDATE USER
+userRouter.put('/:id', verifyUserToken, updateUser);
+// DELETE USER
+userRouter.delete('/:id', verifyUserToken, deleteUser);
+// SUBSCRIBE ON USER
+userRouter.put('/sub/:id', verifyUserToken, subscribeUser);
+// UNSUBSCRIBE ON USER
+userRouter.put('/unsub/:id', verifyUserToken, unsubscribeUser);
+// LIKE VIDEO
+userRouter.put('/like/:videoId', verifyUserToken, like);
+// DISLIKE VIDEO
+userRouter.put('/dislike/:videoId', verifyUserToken, dislike);
