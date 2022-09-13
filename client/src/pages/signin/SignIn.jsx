ace521bd6f9ff149f7f8920c8a8a125ddaae426a
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-09-13 19:10:19 +0300

    Feat: connect BE and FE SignIn

--- client/src/pages/signin/SignIn.jsx
+++ client/src/pages/signin/SignIn.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
 	SignInButton,
 	SignInContainer,
@@ -9,22 +9,51 @@ import {
 } from './SignIn.styled';
 
 import { Hr } from '../../components/UI';
+import axios from 'axios';
 
 export const SignIn = () => {
+	const [formData, setFormData] = useState({});
+
+	const setData = (e) => {
+		const { name, value } = e.target;
+
+		setFormData({ ...formData, [name]: value });
+	};
+
+	const handleLogin = async (e) => {
+		e.preventDefault();
+		const { signin_username: name, signin_password: password } = formData;
+		try {
+			const response = await axios.post('/auth/signin', { name, password });
+			console.log('response :>> ', response.data);
+		} catch (error) {}
+	};
+
 	return (
 		<SignInContainer>
 			<SignInWrapper>
 				<SignInTitle>Sign in</SignInTitle>
 				<SignInSubtitle>to continue to your channel</SignInSubtitle>
-				<SignInInput placeholder='Username' />
-				<SignInInput type='password' placeholder='Password' />
-				<SignInButton>Sign in</SignInButton>
+				<SignInInput name='signin_username' placeholder='Username' onChange={setData} />
+				<SignInInput
+					name='signin_password'
+					type='password'
+					placeholder='Password'
+					onChange={setData}
+				/>
+				<SignInButton onClick={handleLogin}>Sign in</SignInButton>
 				<Hr style={{ margin: '3px' }} />
 				<SignInTitle>or</SignInTitle>
 				<Hr style={{ margin: '3px' }} />
 				<SignInTitle>Sign Up</SignInTitle>
-				<SignInInput placeholder='Username' />
-				<SignInInput type='password' placeholder='Password' />
+				<SignInInput name='signup_username' placeholder='Username' onChange={setData} />
+				<SignInInput name='signup_email' type='email' placeholder='Email' onChange={setData} />
+				<SignInInput
+					name='signup_password'
+					type='password'
+					placeholder='Your password'
+					onChange={setData}
+				/>
 				<SignInInput type='password' placeholder='Repeat your password' />
 				<SignInButton>Sign up</SignInButton>
 			</SignInWrapper>
