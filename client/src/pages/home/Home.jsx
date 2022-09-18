c63f395ebf79d4d51b2bb5bf6f1225ea10e554e1
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-09-18 19:48:11 +0300

    Redirect to the SignIn page if user is not auth and has no access

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,18 +2,26 @@ import React, { useEffect, useState } from 'react';
 import { Card } from '../../components';
 import { HomeContainer } from './Home.styled';
 import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 export const Home = ({ type }) => {
 	const [videos, setVideos] = useState([]);
+	const isAuth = useSelector((state) => !!state.user?.data?._id);
+
+	const navigate = useNavigate();
 
 	useEffect(() => {
+		if (type === 'subscriptions' && !isAuth) {
+			navigate('/signin');
+		}
 		const getVideos = async () => {
 			const response = await axios.get(`/videos/${type}`);
 			setVideos(response.data);
 		};
 
 		getVideos();
-	}, [type]);
+	}, [type, isAuth, navigate]);
 
 	return (
 		<HomeContainer>
