b6c63fc240b3366cc0619c2a5776cbf3c03ff8e9
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-09-29 22:13:37 +0300

    Feat: set to shown Upload modal window

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,13 +1,16 @@
+import { useState } from 'react';
 import { ExitToAppOutlined, Person, SearchOutlined, VideoCallOutlined } from '@mui/icons-material';
-import React from 'react';
 import { LinkBtn, Logo, SCButton, SCLink } from '../UI';
 import { Avatar, NavContainer, NavInput, NavSearch, NavWrapper, User } from './Navbar.styled';
 import LogoImg from '../../assets/logo.webp';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../../redux/userSlice';
 import axios from 'axios';
+import { Upload } from '../upload/Upload';
 
 export const Navbar = ({ children }) => {
+	const [isShown, setIsShown] = useState(false);
+
 	const userData = useSelector((state) => state.user.data);
 	const dispatch = useDispatch();
 
@@ -19,35 +22,39 @@ export const Navbar = ({ children }) => {
 	};
 
 	return (
-		<NavContainer>
-			{children}
-			<NavWrapper>
-				<Logo>
-					<SCLink to='/'>
-						<img src={LogoImg} alt='logo' style={{ height: '25px' }} /> UATube
-					</SCLink>
-				</Logo>
-				<NavSearch>
-					<NavInput placeholder='Search' />
-					<SearchOutlined />
-				</NavSearch>
-				{userData ? (
-					<User>
-						<VideoCallOutlined style={{ cursor: 'pointer' }} />
-						<Avatar src={userData.img} />
-						{userData.name}
-						<SCButton onClick={logOut}>
-							<ExitToAppOutlined />
-							Logout
-						</SCButton>
-					</User>
-				) : (
-					<LinkBtn to='signin'>
-						<Person />
-						SIGN IN
-					</LinkBtn>
-				)}
-			</NavWrapper>
-		</NavContainer>
+		<>
+			<NavContainer>
+				{children}
+				<NavWrapper>
+					<Logo>
+						<SCLink to='/'>
+							<img src={LogoImg} alt='logo' style={{ height: '25px' }} /> UATube
+						</SCLink>
+					</Logo>
+					<NavSearch>
+						<NavInput placeholder='Search' />
+						<SearchOutlined />
+					</NavSearch>
+					{userData ? (
+						<User>
+							<VideoCallOutlined style={{ cursor: 'pointer' }} onClick={() => setIsShown(true)} />
+							<Avatar src={userData.img} />
+							{userData.name}
+							<SCButton onClick={logOut}>
+								<ExitToAppOutlined />
+								Logout
+							</SCButton>
+						</User>
+					) : (
+						<LinkBtn to='signin'>
+							<Person />
+							SIGN IN
+						</LinkBtn>
+					)}
+				</NavWrapper>
+			</NavContainer>
+
+			{isShown && <Upload setIsShown={setIsShown} />}
+		</>
 	);
 };
