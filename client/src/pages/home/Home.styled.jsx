4560a6829bcc8034467e7a201ddaa3673f33236a
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-10-22 17:31:00 +0300

    Style: smaller gap between cards

--- client/src/pages/home/Home.styled.jsx
+++ client/src/pages/home/Home.styled.jsx
@@ -5,4 +5,7 @@ export const HomeContainer = styled.div`
 	grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
 	gap: 25px 20px;
 	padding: 20px;
+	@media (max-width: 767.98px) {
+		gap: 2px 15px;
+	}
 `;
