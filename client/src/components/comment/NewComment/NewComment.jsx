7c68822542958df224d73ab051849d612b500b2f
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-10-23 19:10:52 +0300

    Feat: Add new comment on FE

--- client/src/components/comment/NewComment/NewComment.jsx
+++ client/src/components/comment/NewComment/NewComment.jsx
@@ -3,10 +3,23 @@ import { useSelector } from 'react-redux';
 
 import { NCInput, NCContainer, NCWrapper, NCAddBtn, SendIcon } from './NewComment.styled';
 import { ChannelImg } from '../../../components/UI';
+import { api } from '../../../axios/instance';
+import { toast } from 'react-toastify';
 
-export const NewComment = () => {
+export const NewComment = ({ videoID }) => {
 	const currentUser = useSelector((state) => state.user?.data);
 	const [commentText, setCommentText] = useState('');
+
+	const addComment = async (text, videoId) => {
+		try {
+			const response = await api.post(`/comments`, { text, videoId });
+			console.log('response', response.data);
+			// Put response in local redux commentSlice
+		} catch (error) {
+			toast.warn(error.response.data.message);
+			console.log(error.response.data.message);
+		}
+	};
 
 	return (
 		<NCContainer>
@@ -16,7 +29,7 @@ export const NewComment = () => {
 					placeholder='Add your comment...'
 					onChange={(e) => setCommentText(e.target.value)}
 				/>
-				<NCAddBtn disabled={!commentText.length}>
+				<NCAddBtn disabled={!commentText.length} onClick={() => addComment(commentText, videoID)}>
 					<SendIcon />
 				</NCAddBtn>
 			</NCWrapper>
