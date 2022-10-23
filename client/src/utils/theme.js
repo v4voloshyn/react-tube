b0b3b01b0efb87d2aebea42d2bd847f16e161256
Voloshyn Pavlo <v4voloshyn@gmail.com>
2022-10-23 23:08:17 +0300

    Style: testing external classes

--- client/src/utils/theme.js
+++ client/src/utils/theme.js
@@ -15,3 +15,11 @@ export const lightTheme = {
 	hrColor: '#ebebeb',
 	shadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px',
 };
+
+export const cancelBtn = {
+	backgroundColor: '#1469b4',
+	width: 'content-width',
+	padding: '16px 10px',
+	height: '25px',
+	borderRadius: '15px',
+};
